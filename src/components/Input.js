b9d31bafc7462fc9b5ce6f3a40facd7ed94cd955
moise <moisemlg90@gmail.com>
2022-04-28 13:35:32 +0200

    Add functionalities

---
+++ src/components/Input.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Input = ({
+  type, className, placeholder, name, onChange,
+}) => (
+  <>
+    <input
+      type={type}
+      name={name}
+      className={className}
+      placeholder={placeholder}
+      onChange={onChange}
+    />
+  </>
+);
+
+Input.propTypes = {
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
